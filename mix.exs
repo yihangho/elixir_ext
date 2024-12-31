f548dbc43a3dbf04bd3ce7ffe7d10b04a9b1dc4d
Yihang Ho <hoyihang5@gmail.com>
2024-12-31 18:39:54 +0800

    Install ex_doc

--- mix.exs
+++ mix.exs
@@ -14,11 +14,9 @@ defmodule Ext.MixProject do
     []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.36", only: :dev, runtime: false},
     ]
   end
 end
