7f0ed5d3c0c38794cb51a0bd2131c3b54056a85e
Yihang Ho <hoyihang5@gmail.com>
2025-01-01 22:07:06 +0800

    Prepare for Hex publication

--- mix.exs
+++ mix.exs
@@ -6,7 +6,10 @@ defmodule Ext.MixProject do
       app: :ext,
       version: "0.1.0",
       elixir: "~> 1.16",
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/yihangho/elixir_ext",
     ]
   end
 
@@ -18,6 +21,20 @@ defmodule Ext.MixProject do
     [
       {:ex_doc, "~> 0.36", only: :dev, runtime: false},
       {:mimic, "~> 1.11", only: :test},
+    ]
+  end
+
+  defp description do
+    "An extension to the Elixir standard library"
+  end
+
+  defp package do
+    [
+      name: "ext",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/yihangho/elixir_ext",
+      },
     ]
   end
 end
